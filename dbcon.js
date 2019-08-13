238335729de7746dd4a57269982c89be813513e8
ahuynh0730 <42370685+ahuynh0730@users.noreply.github.com>
2019-08-12 19:09:52 -0500

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_huynhant',
-  password        : 'Affton2012',
+  password        : 'insert_password_here',
   database        : 'cs340_huynhant'
 });
 
