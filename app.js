fc145efd131b1fb470bd55737171b0f240539229
ahuynh0730 <42370685+ahuynh0730@users.noreply.github.com>
2019-08-15 23:14:20 -0500

    Customers shown on add order page

--- app.js
+++ app.js
@@ -117,6 +117,53 @@ app.get('/orders', function(req,res,next) {
 	});
 });
 
+//to add an order
+app.get('/addOrder', function(req, res, next) {
+	var context = {};
+	var createString = ""
+		+ "SELECT " 
+			+ " customers.id AS 'customerId',"
+			+ " CONCAT(customers.fname, ' ', customers.lname) AS 'customerName'"
+		+ " FROM customers";
+	mysql.pool.query(createString, function(err, rows, fields){
+		if (err){
+			next(err);
+			return;
+		}
+		var params = [];
+		for (var row in rows){
+			var addItem = {
+				'customerId':rows[row].customerId,
+				'customerName':rows[row].customerName
+			};
+			params.push(addItem);
+		}
+		
+		/*var employeeString = ""
+			+ "SELECT "
+				+ " employees.id AS 'employeeID',"
+				+ " CONCAT(employees.fname, ' ', employees.lName) AS 'employeeName'"
+			+ " FROM employees";
+		mysql.pool.query(employeeString,function(err,rows,fields){
+			if (err){
+				next(err);
+				return;
+			}	
+			var empParams = [];
+			for (var row in rows){
+				var addItem = {
+					'employeeId':rows[row].employeeId,
+					'employeeName':rows[row].employeeName
+				};
+			empParams.push(addItem);
+			}
+			
+		});*/
+		context.results = params;
+		res.render('addOrders',context);
+	});
+});
+
 //to display all employees
 app.get('/employees', function(req,res,next) {
 	var context = {};
