c3cba6f9857b9fe376b573eb5843ab664cad5e47
Anthony Huynh <ahuynh0730@gmail.com>
2019-09-24 17:17:54 -0500

    My first commit

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_huynhant',
-  password        : 'insert_password_here',
-  database        : 'cs340_huynhant'
+  connectionLimit 	: 10,
+  host            	: 'remotemysql.com',
+  user 				: 'gNDRuQR4wv',
+  password			: '7xGAgwmBDI',
+  database			: 'gNDRuQR4wv' 
 });
 
 module.exports.pool = pool;
